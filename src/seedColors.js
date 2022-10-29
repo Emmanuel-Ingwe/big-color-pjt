952f8645fa30de502a7634aa2e89ed3815851adf
Richard Ingwe <richardingwe5@gmail.com>
2022-10-29 08:21:26 +0100

    ___

---
+++ src/seedColors.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        paletteName: "Material UI Colors",
+        id: "material-ui-colors",
+        emoji: "CL",
+        colors: [
+            { name: "red", color: "#F44336" },
+            { name: "pink", color: "#E91E63" },
+            { name: "purple", color: "#9C27B0" },
+            { name: "deeppurple", color: "#673Ab7" },
+            { name: "indigo", color: "#3f51b5" },
+            { name: "blue", color: "#2196f3" },
+            { name: "lightblue", color: "#03a9f4" },
+            { name: "cyan", color: "#00bcd4" },
+            { name: "teal", color: "#009688" },
+            { name: "green", color: "#4caf50" },
+            { name: "lightgreen", color: "#8bc34a" },
+            { name: "lime", color: "#cddc39" },
+            { name: "yellow", color: "#ffeb3b" },
+            { name: "amber", color: "#ffc107" },
+            { name: "orange", color: "#ff9800" },
+            { name: "deeporange", color: "#ff5722" },
+            { name: "brown", color: "#795548" },
+            { name: "grey", color: "#9e9e9e" },
+            { name: "bluegrey", color: "#607d8b" },
+        ]
+    }
+];
