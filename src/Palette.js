b8853c31127677a4c10db036c70adb8f5612dc78
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2022-11-07 08:38:50 +0100

    Footer-palette

--- src/Palette.js
+++ src/Palette.js
@@ -17,7 +17,7 @@ class Palette extends Component {
         this.setState({ format: val });
     }
     render() {
-        const { colors } = this.props.palette;
+        const { colors, paletteName, emoji } = this.props.palette;
         const { level, format } = this.state;
         const colorBoxes = colors[level].map(color => (
             <ColorBox background={color[format]} name={color.name} />
@@ -26,6 +26,10 @@ class Palette extends Component {
             <div className="Palette">
                 <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}></Navbar>
                 <div className="Palette-colors">{colorBoxes}</div>
+                <footer className="Palette-footer">
+                    {paletteName}
+                    <span className="emoji">{emoji}</span>
+                </footer>
             </div>
         );
     }
